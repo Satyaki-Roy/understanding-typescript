a3dc6b515232a002509b59e77bbcc365c3130685
Satyaki-Roy <satyakiroy001@gmail.com>
2020-09-15 11:07:07 +0530

    enum types

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-// tuples //
+// enum //
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["AUTHOR"] = 1] = "AUTHOR";
+    Role[Role["READ_ONLY"] = 2] = "READ_ONLY";
+})(Role || (Role = {}));
 var person = {
     name: "Satyaki",
     age: 27,
     hobbies: ["football", "cricket", "travelling"],
-    role: [2, "author"]
+    role: Role.ADMIN
 };
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
-    // console.log(hobby.map()); this will through an error
-    // as TS knows map cant be used on strings
-    console.log(hobby.toUpperCase()); // TS knows hobby is a string and hence allows this method
+    console.log(hobby.toUpperCase());
 }
+if (person.role === 0)
+    console.log("Person is a ADMIN user");
--- app.ts
+++ app.ts
@@ -1,19 +1,25 @@
-// tuples //
+// enum //
+
+enum Role {
+  ADMIN,
+  AUTHOR,
+  READ_ONLY,
+}
 
 const person: {
   name: string;
   age: number;
   hobbies: string[];
-  role: [number, string];
+  role: number;
 } = {
   name: "Satyaki",
   age: 27,
   hobbies: ["football", "cricket", "travelling"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
 for (const hobby of person.hobbies) {
-  // console.log(hobby.map()); this will through an error
-  // as TS knows map cant be used on strings
-  console.log(hobby.toUpperCase()); // TS knows hobby is a string and hence allows this method
+  console.log(hobby.toUpperCase());
 }
+
+if (person.role === 0) console.log("Person is a ADMIN user");
