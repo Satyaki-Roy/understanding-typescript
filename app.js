90209b58fa2d23d44f6a3fa2f00332a7e54ee6df
Satyaki-Roy <satyakiroy001@gmail.com>
2020-09-15 09:12:43 +0530

    array types

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // explicitly declaring types in an object
 var person = {
     name: "Satyaki",
-    age: 27
+    age: 27,
+    hobbies: ["football", "cricket", "travelling"]
 };
-console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    // console.log(hobby.map()); this will through an error
+    // as TS knows map cant be used on strings
+    console.log(hobby.toUpperCase()); // TS knows hobby is a string and hence allows this method
+}
--- app.ts
+++ app.ts
@@ -3,9 +3,15 @@
 const person: {
   name: string;
   age: number;
+  hobbies: string[];
 } = {
   name: "Satyaki",
   age: 27,
+  hobbies: ["football", "cricket", "travelling"],
 };
 
-console.log(person.name);
+for (const hobby of person.hobbies) {
+  // console.log(hobby.map()); this will through an error
+  // as TS knows map cant be used on strings
+  console.log(hobby.toUpperCase()); // TS knows hobby is a string and hence allows this method
+}
