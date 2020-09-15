ee1eb2f1f39896a49a800d3e35c5abcd1153edec
Satyaki-Roy <satyakiroy001@gmail.com>
2020-09-15 07:54:03 +0530

    using types

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log("Your code goes here...");
+function add(num1, num2) {
+    return num1 + num2;
+}
+var result = add(5, 2.8);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,7 @@
-console.log("Your code goes here...");
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+const result = add(5, 2.8);
+
+console.log(result);
