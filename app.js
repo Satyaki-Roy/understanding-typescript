ba08cb48e6bbf3da03ab0df51de0602e285a6480
Satyaki-Roy <satyakiroy001@gmail.com>
2020-09-15 14:31:00 +0530

    literal types

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-function combine(arg1, arg2) {
-    return typeof arg1 === "number" && typeof arg2 === "number"
-        ? arg1 + arg2
-        : arg1.toString() + arg2.toString();
+function combine(arg1, arg2, result) {
+    if (result === "as-number") {
+        return +arg1 + +arg2;
+    }
+    else if (result === "as-text") {
+        return arg1.toString() + arg2.toString();
+    }
 }
-console.log(combine(5, 8));
-console.log(combine("Hello", "World"));
-console.log(combine("Rank ", 1));
+console.log(combine(5, 8, "as-number"));
+console.log(combine(5, 8, "as-text"));
+console.log(combine("5", 8, "as-number"));
+console.log(combine("5", 8, "as-text"));
+console.log(combine("Hello ", "World", "as-number"));
+console.log(combine("Hello ", "World", "as-text"));
+console.log(combine("Rank ", 1, "as-number"));
+console.log(combine("Rank ", 1, "as-text"));
--- app.ts
+++ app.ts
@@ -1,9 +1,20 @@
-function combine(arg1: number | string, arg2: number | string) {
-  return typeof arg1 === "number" && typeof arg2 === "number"
-    ? arg1 + arg2
-    : arg1.toString() + arg2.toString();
+function combine(
+  arg1: number | string,
+  arg2: number | string,
+  result: "as-number" | "as-text"
+) {
+  if (result === "as-number") {
+    return +arg1 + +arg2;
+  } else if (result === "as-text") {
+    return arg1.toString() + arg2.toString();
+  }
 }
 
-console.log(combine(5, 8));
-console.log(combine("Hello", "World"));
-console.log(combine("Rank ", 1));
+console.log(combine(5, 8, "as-number"));
+console.log(combine(5, 8, "as-text"));
+console.log(combine("5", 8, "as-number"));
+console.log(combine("5", 8, "as-text"));
+console.log(combine("Hello ", "World", "as-number"));
+console.log(combine("Hello ", "World", "as-text"));
+console.log(combine("Rank ", 1, "as-number"));
+console.log(combine("Rank ", 1, "as-text"));
