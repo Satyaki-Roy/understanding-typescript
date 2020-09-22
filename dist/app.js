086ffb0a87eca687c3f77a4094afca25c8211830
Satyaki-Roy <satyakiroy001@gmail.com>
2020-09-22 17:33:29 +0530

    type guards

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,45 @@
 "use strict";
-var e1 = {
-    name: "Satyaki",
-    privileges: ["create-server"],
-    startDate: new Date(),
-};
-console.log(e1);
-var test = 25;
+function infoOfEmployee(emp) {
+    console.log(emp.name);
+    if ("privileges" in emp) {
+        console.log(emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log(emp.startDate);
+    }
+}
+var Car = (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log("Driving a car......");
+    };
+    return Car;
+}());
+var Truck = (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log("Driving a truck......");
+    };
+    Truck.prototype.load = function () {
+        console.log("Carrying some load......");
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+function vehicleInfo(obj) {
+    obj.drive();
+    if (obj instanceof Truck) {
+        obj.load();
+    }
+}
+vehicleInfo(v1);
+vehicleInfo(v2);
+function addTypeGuard(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
--- src/app.ts
+++ src/app.ts
@@ -8,22 +8,60 @@ type Employee = {
   startDate: Date;
 };
 
-type ElevatedEmployee = Admin & Employee;
+type NotAnEmployee = Admin | Employee;
 
-const e1: ElevatedEmployee = {
-  name: "Satyaki",
-  privileges: ["create-server"],
-  startDate: new Date(),
-};
+function infoOfEmployee(emp: NotAnEmployee) {
+  console.log(emp.name);
+  if ("privileges" in emp) {
+    console.log(emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log(emp.startDate);
+  }
+}
 
-console.log(e1);
+/////////////////////////////////////////////////////
+
+class Car {
+  drive() {
+    console.log("Driving a car......");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck......");
+  }
+
+  load() {
+    console.log("Carrying some load......");
+  }
+}
+
+type CarOrTruck = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function vehicleInfo(obj: CarOrTruck) {
+  obj.drive();
+  // if ("load" in obj) {
+  // OR //
+  if (obj instanceof Truck) {
+    obj.load();
+  }
+}
+
+vehicleInfo(v1);
+vehicleInfo(v2);
 
 /////////////////////////////////////////////////////
 
 type Combinable = string | number;
-type Numeric = number | boolean;
 
-type Universal = Combinable & Numeric;
-
-// only number is allowed
-const test: Universal = 25;
+function addTypeGuard(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
