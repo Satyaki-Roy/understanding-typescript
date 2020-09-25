a936166f5fd20d0980bf524623bab68d3cec4f93
Satyaki-Roy <satyakiroy001@gmail.com>
2020-09-25 10:17:32 +0530

    generic function

--- dist/app.js
+++ dist/app.js
@@ -4,3 +4,13 @@ function merge(objA, objB) {
 }
 const mergedObj = merge({ name: "Satyaki" }, { age: 27 });
 console.log(mergedObj.age);
+function countAndDescribe(element) {
+    let descriptionText = `Got no value.`;
+    if (element.length === 1) {
+        descriptionText = `Got 1 element.`;
+    }
+    else if (element.length > 1) {
+        descriptionText = `Got ${element.length} element.`;
+    }
+    return [element, descriptionText];
+}
